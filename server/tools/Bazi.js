6e525b73aea945a8c688dc089058abb93865dfc8
zichong.zhj <zichong.zhj@alibaba-inc.com>
2018-03-10 18:57:59 +0800

    计算八字及五行

---
+++ server/tools/Bazi.js
@@ -0,0 +1,289 @@
+/**
+ * 八字的计算
+ */
+
+//引入计算阴历，及八字的日期工具
+const chineseDateUtils = require("./dateUtils.js")
+
+//一天内时间的干支
+const TimeGanZhiTable = [
+        ["甲子", "丙子", "戊子", "庚子", "壬子" ],
+        ["乙丑", "丁丑", "己丑", "辛丑", "癸丑" ],
+        ["丙寅", "戊寅", "庚寅", "壬寅", "甲寅" ],
+        ["丁卯", "己卯", "辛卯", "癸卯", "乙卯" ],
+        ["戊辰", "庚辰", "壬辰", "甲辰", "丙辰" ],
+        ["己巳", "辛巳", "癸巳", "己巳", "丁巳" ],
+        ["庚午", "壬午", "甲午", "丙午", "戊午" ],
+        ["辛未", "癸未", "乙未", "丁未", "己未" ],
+        ["壬申", "甲申", "丙申", "戊申", "庚申" ],
+        ["癸酉", "乙酉", "丁酉", "己酉", "辛酉" ],
+        ["甲戌", "丙戌", "戊戌", "庚戌", "壬戌" ],
+        ["乙亥", "丁亥", "己亥", "辛亥", "癸亥" ]
+    ];
+const TianGan = "甲乙丙丁戊己庚辛壬癸";
+const DiZhi = "子丑寅卯辰巳午未申酉戌亥";
+/**
+   天干地支的五行属性表
+    天干： 甲-木、乙-木、丙-火、丁－火、戊－土、己－土、庚－金、辛－金、壬－水、癸－水
+    地支： 子-水、丑-土、寅-木、卯－木、辰－土、巳－火、午－火、未－土、申－金、酉－金、戌－土、亥－水
+*/
+const TianGan_WuXingProp = [1, 1, 3, 3, 4, 4, 0, 0, 2, 2];
+const DiZhi_WuXingProp   = [2, 4, 1, 1, 4, 3, 3, 4, 0, 0, 4, 2 ];
+/**
+   金 --- 0 
+   木 --- 1
+   水 --- 2
+   火 --- 3
+   土 --- 4
+*/
+const WuXingTable = [ '金', '木', '水', '火', '土' ];
+/**
+  五行相生关系表
+  金生水，水生木，木生火，火生土，土生金
+*/
+const GenerationSourceTable = [ 4, 2, 0, 1, 3];
+
+const TianGan_Strength = [
+        [ 1.2,  1.2,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.2,  1.2 ],
+        [ 1.06, 1.06, 1.0,  1.0,  1.1,  1.1,  1.14, 1.14, 1.1,  1.1 ],
+        [ 1.14, 1.14, 1.2,  1.2,  1.06, 1.06, 1.0,  1.0,  1.0,  1.0 ],
+        [ 1.2,  1.2,  1.2,  1.2,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0 ],
+        [ 1.1,  1.1,  1.06, 1.06, 1.1,  1.1,  1.1,  1.1,  1.04, 1.04],
+        [ 1.0,  1.0,  1.14, 1.14, 1.14, 1.14, 1.06, 1.06, 1.06, 1.06],
+        [ 1.0,  1.0,  1.2,  1.2,  1.2,  1.2,  1.0,  1.0,  1.0,  1.0 ],
+        [ 1.04, 1.04, 1.1,  1.1,  1.16, 1.16, 1.1,  1.1,  1.0,  1.0 ],
+        [ 1.06, 1.06, 1.0,  1.0,  1.0,  1.0,  1.14, 1.14, 1.2,  1.2 ],
+        [ 1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.2,  1.2,  1.2,  1.2 ],
+        [ 1.0,  1.0,  1.04, 1.04, 1.14, 1.14, 1.16, 1.16, 1.06, 1.06],
+        [ 1.2,  1.2,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.14, 1.14]
+    ];
+const DiZhi_Strength =
+  [
+    new ZiStrength('子', '癸', [1.2,  1.1,   1.0,   1.0,  1.04,  1.06,  1.0,  1.0,   1.2,   1.2, 1.06, 1.14] ),
+    new ZiStrength('丑', '癸', [0.36, 0.33,  0.3,   0.3,  0.312, 0.318, 0.3,  0.3,   0.36,  0.36, 0.318, 0.342]),
+    new ZiStrength('丑', '辛', [0.2,  0.228, 0.2,   0.2,  0.23,  0.212, 0.2,  0.22,  0.228, 0.248, 0.232, 0.2]),
+    new ZiStrength('丑', '己', [0.5,  0.55,  0.53,  0.5,  0.55,  0.57,  0.6,  0.58,  0.5,   0.5, 0.57, 0.5 ]),
+    new ZiStrength('寅', '丙', [0.3,  0.3,   0.36,  0.36, 0.318, 0.342, 0.36, 0.33,  0.3,   0.3, 0.342, 0.318]),
+    new ZiStrength('寅', '甲', [0.84, 0.742, 0.798, 0.84, 0.77,  0.7,   0.7,  0.728, 0.742, 0.7, 0.7, 0.84 ]),
+    new ZiStrength('卯', '乙', [1.2,  1.06,  1.14,  1.2,  1.1,   1.0,   1.0,  1.04,  1.06,  1.0, 1.0, 1.2 ]),
+    new ZiStrength('辰', '乙', [0.36, 0.318, 0.342, 0.36, 0.33,  0.3,   0.3,  0.312, 0.318, 0.3, 0.3, 0.36 ]),
+    new ZiStrength('辰', '癸', [0.24, 0.22,  0.2,   0.2,  0.208, 0.2,   0.2,  0.2,   0.24,  0.24, 0.212, 0.228]),
+    new ZiStrength('辰', '戊', [0.5,  0.55,  0.53,  0.5,  0.55,  0.6,   0.6,  0.58,  0.5,   0.5, 0.57, 0.5 ]),
+    new ZiStrength('巳', '庚', [0.3,  0.342, 0.3,   0.3,  0.33,  0.3,   0.3,  0.33,  0.342, 0.36, 0.348, 0.3 ]),
+    new ZiStrength('巳', '丙', [0.7,  0.7,   0.84,  0.84, 0.742, 0.84,  0.84, 0.798, 0.7,   0.7, 0.728, 0.742 ]),
+    new ZiStrength('午', '丁', [1.0,  1.0,   1.2,   1.2,  1.06,  1.14,  1.2,  1.1,   1.0,   1.0, 1.04, 1.06 ]),
+    new ZiStrength('未', '丁', [0.3,  0.3,   0.36,  0.36, 0.318, 0.342, 0.36, 0.33,  0.3,   0.3, 0.312, 0.318 ]),
+    new ZiStrength('未', '乙', [0.24, 0.212, 0.228, 0.24, 0.22,  0.2,   0.2,  0.208, 0.212, 0.2, 0.2, 0.24 ]),
+    new ZiStrength('未', '己', [0.5,  0.55,  0.53,  0.5,  0.55,  0.57,  0.6,  0.58,  0.5,   0.5, 0.57, 0.5 ]),
+    new ZiStrength('申', '壬', [0.36, 0.33,  0.3,   0.3,  0.312, 0.318, 0.3,  0.3,   0.36,  0.36, 0.318, 0.342 ]),
+    new ZiStrength('申', '庚', [0.7,  0.798, 0.7,   0.7,  0.77,  0.742, 0.7,  0.77,  0.798, 0.84, 0.812, 0.7 ]),
+    new ZiStrength('酉', '辛', [1.0,  1.14,  1.0,   1.0,  1.1,   1.06,  1.0,  1.1,   1.14,  1.2, 1.16, 1.0 ]),
+    new ZiStrength('戌', '辛', [0.3,  0.342, 0.3,   0.3,  0.33,  0.318, 0.3,  0.33,  0.342, 0.36, 0.348, 0.3 ]),
+    new ZiStrength('戌', '丁', [0.2,  0.2,   0.24,  0.24, 0.212, 0.228, 0.24, 0.22,  0.2,   0.2, 0.208, 0.212 ]),
+    new ZiStrength('戌', '戊', [0.5,  0.55,  0.53,  0.5,  0.55,  0.57,  0.6,  0.58,  0.5,   0.5, 0.57, 0.5 ]),
+    new ZiStrength('亥', '甲', [0.36, 0.318, 0.342, 0.36, 0.33,  0.3,   0.3,  0.312, 0.318, 0.3, 0.3, 0.36 ]),
+    new ZiStrength('亥', '壬', [0.84, 0.77,  0.7,   0.7,  0.728, 0.742, 0.7,  0.7,   0.84,  0.84, 0.724, 0.798 ])
+  ];
+function ZiStrength(gan, zhi, strengh){
+  this.diZhi=gan;
+  this.zhiCang = zhi;
+  this.strength = strengh;
+}
+
+/**
+ * 计算八字及五行属性的类
+ * BaziComputer
+ */
+function BaziComputer(year, month, day, hour) {
+  this._year = year;
+  this._month = month;
+  this._day = day;
+  this._hour = hour;
+  this._chineseDate = new ChineseDate(year, month, day);
+  this._sixBaZi = this._chineseDate.getGanZhiString();
+  //计算最终的八字
+  this._baZi = computeTimeGan(this._sixBaZi, hour);
+  //计算五行
+  this._wuxing = calculateBazi(this._baZi);
+}
+
+/**
+ * 获取阴历的年月日
+ */
+BaziComputer.prototype.getChineseDateString = function () {
+  return this._chineseDate.getChineseDateString();
+}
+
+/**
+ * 获取八字
+ */
+BaziComputer.prototype.getBazi = function () {
+  return this._baZi;
+}
+/**
+ * 获取五行描述
+ */
+BaziComputer.prototype.getWuxing = function(){
+  return this._wuxing;
+}
+
+/**
+     * 根据八字计算五行平衡
+     * 输入参数：bazi，年月日时的干支，即俗称的八字
+     * 输出结果：分析结果字符串，Unicode编码
+     * @param bazi
+     * @return
+     */
+BaziComputer.prototype.calculateBazi = function(bazi) {
+  var strengthResult = new double[5];
+  var monthIndex = computeZhiIndex(bazi.charAt(3));
+  if (monthIndex == -1) {
+    return null;
+  }
+
+  var sResultBuf = new StringBuffer();
+  sResultBuf.append(bazi).append("\n\n");
+
+  for (var wuXing = 0; wuXing < 5; wuXing++) {
+    var value1 = 0.0, value2 = 0.0;
+    var i;
+    //扫描4个天干
+    for (i = 0; i < 8; i += 2) {
+      var gan = bazi.charAt(i);
+      var index = computeGanIndex(gan);
+      if (index == -1) {
+        return null;
+      }
+      if (TianGan_WuXingProp[index] == wuXing) {
+        value1 += TianGan_Strength[monthIndex][index];
+      }
+    }
+    //扫描支藏
+    for (i = 1; i < 8; i += 2) {
+      var zhi = bazi.charAt(i);
+      for (var j = 0; j < DiZhi_Strength.length; j++) {
+        if (DiZhi_Strength[j].diZhi == zhi) {
+          var zhiCangIndex = computeGanIndex(DiZhi_Strength[j].zhiCang);
+          if (zhiCangIndex == -1) {
+            return null;
+          }
+          if (TianGan_WuXingProp[zhiCangIndex] == wuXing) {
+            value2 += DiZhi_Strength[j].strength[monthIndex];
+            break;
+          }
+        }
+      }
+    }
+
+    strengthResult[wuXing] = value1 + value2;
+    //输出一行计算结果
+    {
+      var tmpBuf = String.format(":%.3f + %.3f = %.3f\n", value1,value2, (value1 + value2));
+      sResultBuf.append(WuXingTable[wuXing]);
+      sResultBuf.append(":\t");
+      sResultBuf.append(tmpBuf);
+    }
+  }
+
+  //根据日干求命里属性
+  var fateProp, srcProp;
+  {
+    fateProp = TianGan_WuXingProp[computeGanIndex(bazi.charAt(4))];
+    if (fateProp == -1) {
+      return null;
+    }
+    sResultBuf.append("\n命属");
+    sResultBuf.append(WuXingTable[fateProp]);
+    sResultBuf.append("\n\n");
+  }
+
+  //求同类和异类的强度值
+  srcProp = GenerationSourceTable[fateProp];
+  {
+    var tongLei = strengthResult[fateProp] + strengthResult[srcProp];
+    var yiLei = 0.0;
+
+    for (var i = 0; i < 5; i++){
+      yiLei += strengthResult[i];
+    }
+    yiLei -= tongLei;
+    var tmpBuf = String.format("%.3f + %.3f = %.3f\n", strengthResult[fateProp],
+      strengthResult[srcProp], tongLei);
+
+    sResultBuf.append("同类：")
+      .append(WuXingTable[fateProp])
+      .append("+")
+      .append(WuXingTable[srcProp])
+      .append("，");
+    sResultBuf.append(tmpBuf);
+    //
+    tmpBuf = String.format("%.3f\n", yiLei);
+    sResultBuf.append("异类：总和为 ");
+    sResultBuf.append(tmpBuf);
+  }
+
+  return sResultBuf.toString();
+}
+
+/**
+ * 计算干的位置
+ * @param gan
+ * @return
+ */
+BaziComputer.prototype.computeGanIndex = function (gan) {
+  var i = 0;
+  for (i = 0; i < 10; i++) {
+    if (TianGan.charAt(i) == gan) {
+      break;
+    }
+  }
+  if (i >= 10) {
+    return -1;
+  }
+  return i;
+}
+
+/**
+ * 计算支的位置
+ * @param zhi
+ * @return
+ */
+BaziComputer.prototype.computeZhiIndex = function(zhi) {
+  var i = 0;
+  for (i = 0; i < 12; i++) {
+    if (DiZhi.charAt(i) == zhi) {
+      break;
+    }
+  }
+  if (i >= 12) {
+    return -1;
+  }
+  return i;
+}
+
+/**
+ * 根据出生年月日的干支计算时辰干支
+ * 输入参数：bazi，年月日的干支，即八字中的前六个字
+ * 输入参数：hour，出生时间的小时数，-1~22
+ * 输出结果：八字字符串，Unicode编码
+ */
+BaziComputer.prototype.computeTimeGan = function( bazi,  hour) {
+  var dayGan = bazi.charAt(4);
+  var indexX, indexY;
+  var i;
+  for (i = 0; i < 10; i++) {
+    if (dayGan == TianGan.charAt(i)) {
+      break;
+    }
+  }
+  if (i >= 10) {
+    return null;
+  }
+  indexX = i;
+  if (indexX >= 5) {
+    indexX -= 5;
+  }
+  indexY = (hour + 1) / 2;
+  return bazi + TimeGanZhiTable[indexY][indexX];
+}
+
--- server/tools/dateUtils.js
+++ server/tools/dateUtils.js
@@ -1,3 +1,261 @@
 /**
- * data的处理逻辑
+ * 需要用到的常量声明
  */
+const MinYear = 1900;  //农历年
+const MaxYear = 2050;  //农历年
+// 阳历的日期
+const MinDay = new Date(1900, 1, 30, 0, 0, 0, 0);
+const MaxDay = new Date(2049, 12, 31, 0, 0, 0, 0);
+const HZNum = "零一二三四五六七八九";
+
+/**
+ * 下面是经过整理的150年内的阴历数据：
+ * 共15行，每行10个数据。每个数据代表一年，从阳历1900.1.31日起，为第一个数据年的开始，即阳历1900.1.31＝阴历0.1.1。
+ * 150个数据可推150年的阴历，因此目前最大只能推算到2049年，以后的推导，还需要从天文台得到新的数据后才能推导，否则将出现误差。
+ */
+const LunarDateArray = new Array(
+    0x04BD8, 0x04AE0, 0x0A570, 0x054D5, 0x0D260, 0x0D950, 0x16554, 0x056A0, 0x09AD0, 0x055D2,
+    0x04AE0, 0x0A5B6, 0x0A4D0, 0x0D250, 0x1D255, 0x0B540, 0x0D6A0, 0x0ADA2, 0x095B0, 0x14977,
+    0x04970, 0x0A4B0, 0x0B4B5, 0x06A50, 0x06D40, 0x1AB54, 0x02B60, 0x09570, 0x052F2, 0x04970,
+    0x06566, 0x0D4A0, 0x0EA50, 0x06E95, 0x05AD0, 0x02B60, 0x186E3, 0x092E0, 0x1C8D7, 0x0C950,
+    0x0D4A0, 0x1D8A6, 0x0B550, 0x056A0, 0x1A5B4, 0x025D0, 0x092D0, 0x0D2B2, 0x0A950, 0x0B557,
+    0x06CA0, 0x0B550, 0x15355, 0x04DA0, 0x0A5B0, 0x14573, 0x052B0, 0x0A9A8, 0x0E950, 0x06AA0,
+    0x0AEA6, 0x0AB50, 0x04B60, 0x0AAE4, 0x0A570, 0x05260, 0x0F263, 0x0D950, 0x05B57, 0x056A0,
+    0x096D0, 0x04DD5, 0x04AD0, 0x0A4D0, 0x0D4D4, 0x0D250, 0x0D558, 0x0B540, 0x0B6A0, 0x195A6,
+    0x095B0, 0x049B0, 0x0A974, 0x0A4B0, 0x0B27A, 0x06A50, 0x06D40, 0x0AF46, 0x0AB60, 0x09570,
+    0x04AF5, 0x04970, 0x064B0, 0x074A3, 0x0EA50, 0x06B58, 0x055C0, 0x0AB60, 0x096D5, 0x092E0,
+    0x0C960, 0x0D954, 0x0D4A0, 0x0DA50, 0x07552, 0x056A0, 0x0ABB7, 0x025D0, 0x092D0, 0x0CAB5,
+    0x0A950, 0x0B4A0, 0x0BAA4, 0x0AD50, 0x055D9, 0x04BA0, 0x0A5B0, 0x15176, 0x052B0, 0x0A930,
+    0x07954, 0x06AA0, 0x0AD50, 0x05B52, 0x04B60, 0x0A6E6, 0x0A4E0, 0x0D260, 0x0EA65, 0x0D530,
+    0x05AA0, 0x076A3, 0x096D0, 0x04BD7, 0x04AD0, 0x0A4D0, 0x1D0B6, 0x0D250, 0x0D520, 0x0DD45,
+    0x0B5A0, 0x056D0, 0x055B2, 0x049B0, 0x0A577, 0x0A4B0, 0x0AA50, 0x1B255, 0x06D20, 0x0ADA0,
+    0x14B63
+);
+
+// 干支转换的数据
+const GanZhiStartYear = 1864; //阴历年，干支起始的年份
+const GanZhiStartDay = new Date(1899, 12, 22, 0, 0, 0, 0); //阳历年月日，干支起始的日期
+const GanStr = "甲乙丙丁戊己庚辛壬癸";
+const ZhiStr = "子丑寅卯辰巳午未申酉戌亥";
+
+/**
+ * 中国日期，构造函数 
+ * year  阳历年 int
+ * month 阳历月 int
+ * day   阳历日 int
+ */
+function ChineseDate(year, month, day) {
+  this._datetime = new Date(year, month, day, 0, 0, 0, 0);
+  this.year = year;
+  this.month = month;
+  this.day = day;
+  //阴历的月日
+  this._cYear = year;
+  this._cMonth = month;
+  this._cDay = day;
+
+  //设置转换阴历
+  //转换年， 计算两天的基本差距[即1900到当天的天差]
+  var offsetDay = (this._datetime - MinDay) / (24 * 60 * 60 * 1000); 
+  var temp = 0;  
+  var last = 0;
+  for (this._cYear = MinYear; this._cYear <= MaxYear; this._cYear++) {
+    temp = getChineseYearDays(this._cYear);  //求当年农历年天数
+    last = offsetDay - temp;
+    if (last < 1) {
+      break;
+    } else {
+      offsetDay = last;
+    }
+  }
+
+  this._cIsLeapYear = false; //当月是否闰月
+  var leapMonth = getChineseLeapMonth(this._cYear);//计算该年闰哪个月
+  //设定当年是否有闰月
+  if (leapMonth > 0) {
+    _cIsLeapYear = true;
+  } 
+  //转换阴历月 与 阴历日
+  this._cIsLeapMonth = false;  //当月是否闰月
+  var idxMonth;
+  for (idxMonth = 1; idxMonth <= 12; idxMonth++) {
+    //闰月
+    if (leapMonth > 0
+      && idxMonth == (leapMonth + 1)
+      && this._cIsLeapMonth == false) {
+      this._cIsLeapMonth = true;
+      idxMonth = idxMonth - 1;
+      temp = getChineseLeapMonthDays(this._cYear); //计算闰月天数
+    } else {
+      this._cIsLeapMonth = false;
+      temp = getChineseMonthDays(this._cYear, idxMonth);//计算非闰月天数
+    }
+    last = offsetDay - temp;
+    if (last <= 0) {
+      break;
+    } else {
+      offsetDay = last;
+    }
+  }
+  this._cMonth = idxMonth;
+  this._cDay = offsetDay;
+  //计算干支年月日
+  this._ganZhi = calculateGanZhiString();
+}
+
+/**
+ * 返回干支纪年
+ */
+ChineseDate.prototype.getGanZhiString = function () {
+  return this._ganZhi;
+}
+
+/**
+ * 计算干支纪年
+ */
+ChineseDate.prototype.calculateGanZhiString = function () {
+  //干支年的计算
+  var offsetYear = (this._cYear - GanZhiStartYear) % 60; //计算干支
+  var ganZhiYear = GanStr.charAt(offsetYear % 10) + "" + ZhiStr.charAt(offsetYear % 12);
+  
+  //干支月的计算
+  //根据当年的干支年的干来计算月干的第一个
+  var ganIndex = 1;
+  switch (offsetYear % 10) {
+    case 0: //甲
+      ganIndex = 3;
+      break;
+    case 1: //乙
+      ganIndex = 5;
+      break;
+    case 2: //丙
+      ganIndex = 7;
+      break;
+    case 3: //丁
+      ganIndex = 9;
+      break;
+    case 4: //戊
+      ganIndex = 1;
+      break;
+    case 5: //己
+      ganIndex = 3;
+      break;
+    case 6: //庚
+      ganIndex = 5;
+      break;
+    case 7: //辛
+      ganIndex = 7;
+      break;
+    case 8: //壬
+      ganIndex = 9;
+      break;
+    case 9: //癸
+      ganIndex = 1;
+      break;
+  }
+  var ganMonth = GanStr.charAt((ganIndex + this._cMonth - 2) % 10);
+  //每个月的地支总是固定的,而且总是从寅月开始
+  var zhiIndex;
+  if (this._cMonth > 10) {
+    zhiIndex = this._cMonth - 10;
+  } else {
+    zhiIndex = this._cMonth + 2;
+  }
+  var zhiMonth = ZhiStr.charAt(zhiIndex - 1);
+  //干支月
+  var ganZhiMonth = ganMonth + "" + zhiMonth;
+
+  //干支日的计算
+  var offsetDay = (this._datetime - GanZhiStartDay) / (24 * 60 * 60 * 1000); 
+  var ganZhiOffset = offsetDay % 60;
+  var ganZhiDay = GanStr.charAt(ganZhiOffset % 10) + "" + ZhiStr.charAt(ganZhiOffset % 12);
+  //最终干支字符串
+  return ganZhiYear + ganZhiMonth + ganZhiDay;
+}
+
+/**
+ * 获取阴历的年月日
+ * 农历:2018年闰1月3日
+ */
+ChineseDate.prototype.getChineseDateString = function() {
+  if (this._cIsLeapMonth == true) {
+    return "农历:" + this._cYear + "年闰" + this._cMonth + "月" + this._cDay + "日";
+  } else {
+    return "农历:" + this._cYear + "年"   + this._cMonth + "月" + this._cDay + "日";
+  }
+}
+
+/**
+ *
+ * 返回农历 y年m月的总天数
+ * year  农历年
+ * month 农历月
+ */
+ChineseDate.prototype.getChineseMonthDays = function(year, month){
+  //0X0FFFF[0000 {1111 1111 1111} 1111]
+  if (bitTest32((LunarDateArray[year - MinYear] & 0x0000FFFF), (16 - month))) {
+    return 30;
+  } else {
+    return 29;
+  }
+}
+/**
+ * 测试某位是否为真
+ * num        第几位
+ * bitpostion 位串
+ */
+ChineseDate.prototype.bitTest32 = function(num, bitpostion){
+  var bit = 1 << bitpostion;
+  if ((num & bit) == 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/**
+ * 求当年农历年天数
+ * year 是农历年
+ */
+ChineseDate.prototype.getChineseYearDays = function(year){
+  var sumDay = 348; //29天 X 12个月
+  var i = 0x8000;
+  var info = LunarDateArray[year - MinYear] & 0x0FFFF;
+  //0x04BD8  & 0x0FFFF 中间12位，即4BD，每位代表一个月，为1则为大月，为0则为小月
+  //计算12个月中有多少天为30天
+  var f = 0;
+  for (var m = 0; m < 12; m++) {
+    f = info & i; // 0x04BD8  & 0x0FFFF  & 0x8000[1000 0000 0000 0000]
+    if (f != 0) {
+      sumDay++;
+    }
+    i = i >> 1;
+  }
+  return sumDay + getChineseLeapMonthDays(year);
+}
+/**
+ * 返回农历 y年闰月的天数
+ * year 是农历年
+ */
+ChineseDate.prototype.getChineseLeapMonthDays = function(year) {
+  if (getChineseLeapMonth(year) != 0) {
+    //前4位，即0在这一年是润年时才有意义，它代表这年润月的大小月。
+    if ((LunarDateArray[year - MinYear] & 0x10000) != 0) {
+      //为1则润大月
+      return 30;
+    } else {
+      //为0则润小月
+      return 29;
+    }
+  } else {
+    return 0;
+  }
+}
+/**
+ * 返回农历 y年闰哪个月 1-12 , 没闰传回 0
+ * year 是农历年
+ */
+ChineseDate.prototype.getChineseLeapMonth = function(year) {
+  //最后4位，即8，代表这一年的润月月份，为0则不润。首4位要与末4位搭配使用
+  return LunarDateArray[year - MinYear] & 0xF;
+}
